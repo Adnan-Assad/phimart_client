e50300d5e8d831da67f9d34ce70022761746433b
Adnan Assad <adnnn336@gmail.com>
2026-01-18 11:21:44 +0600

    final added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     target: "esnext",
+//   },
+// });
+// vite.config.js
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    target: "esnext",
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        require("autoprefixer"),
+        // optional: ignore unknown at-rules
+        require("postcss-safe-parser"),
+      ],
+    },
   },
 });
